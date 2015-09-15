85395f4de3f0b7d53d999955fe8681ae5baa8f9c
ron1ka <veranika.sab@gmail.com>
2015-09-15 22:08:45 +0300

    Removed useless commented code and images, renamed images and updated map - 1.5 hours

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -51,36 +51,6 @@ jQuery(document).ready(function($) {
 	-------------------------------------------------------------------*/
 
 
-
-
-	/* Time Countdown 
-	-------------------------------------------------------------------*/
-	$('#time_countdown').countDown({
-        
-        // targetDate: {
-        //     'day': 30,
-        //     'month': 9,
-        //     'year': 2015,
-        //     'hour': 0,
-        //     'min': 0,
-        //     'sec': 0
-        // },
-        // omitWeeks: true
-
-         targetOffset: {
-            'day':      0,
-            'month':    0,
-            'year':     1,
-            'hour':     0,
-            'min':      0,
-            'sec':      3
-		},
-		omitWeeks: true
-
-	    });
-
-
-
 	/* Next Section   
 	-------------------------------------------------------------------*/
 	$('.next-section .go-to-about').click(function() {
